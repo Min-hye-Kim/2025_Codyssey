2a2f9559aff57fd8ac084479e51f7a0e6fdcb9aa
[Min-hye-Kim] <kimilu@ewha.ac.kr>
2025-07-24 03:11:20 +0900

    1.5 완료

--- 1.5/sort_calculator.py
+++ 1.5/sort_calculator.py
@@ -1,15 +1,27 @@
 def main():
 
-    numbers = list(input("숫자를 입력하세요: ").split())
+    numbers = list(map(float, input("숫자를 입력하세요: ").split()))
+    # 리스트 mapping 공부
+
+#————————————————————————————————
+
     def Sorting():
+
         try:
             for i in range(len(numbers)):
-                if numbers[j]>numbers[j+1]:
-                    numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-            print("Sorted:", Sorting)
+                for j in range(i):
+                    if numbers[j]>numbers[j+1]:
+                        numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                        # 리스트 안 요소를 지정하는 방법? 가리키는 방법 알아두기
+            return("Sorted:", *numbers)
+                # 구문에서는 항상 들여쓰기 주의!!!!
+
         except ValueError:
             print ("Invalid input.")
 
+#————————————————————————————————
+
+    print(Sorting())
 
 if __name__ == "__main__":
     main()
