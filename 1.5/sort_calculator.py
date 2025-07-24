4ffd4facced663135d20908fca62440b86ca56e3
[Min-hye-Kim] <kimilu@ewha.ac.kr>
2025-07-24 21:53:49 +0900

    Revert "Edit port"
    This reverts commit 10a214d1624dda40ca5e263b797a9d7fd8861794.

--- 1.5/sort_calculator.py
+++ 1.5/sort_calculator.py
@@ -9,21 +9,22 @@ def main():
 
         try:
             for i in range(len(numbers)):
-                for j in range(len(numbers) - i - 1):  # ← 여기 중요
+                for j in range(i):
                     if numbers[j]>numbers[j+1]:
                         numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
                         # 리스트 안 요소를 지정하는 방법? 가리키는 방법 알아두기
-            return "Sorted:", *numbers
-                # loop 구문에서는 항상 들여쓰기 주의!!!!
-                # print( f"Sorted:{*numbers}") → 문법적으로 금지된 형태?
+            return("Sorted:", *numbers)
+                # 구문에서는 항상 들여쓰기 주의!!!!
 
         except ValueError:
             print ("Invalid input.")
 
-    print(*Sorting())
-    #여기 좀 헷갈림.. 앞에서 * 붙여줬는데 또 해줘야 되는지?
-
 #————————————————————————————————
+
+    print(Sorting())
 
 if __name__ == "__main__":
     main()
+
+# git commit 하는 미션용 추가 주석
+#————————————————————————————————
