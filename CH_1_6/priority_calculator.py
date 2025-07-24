4658151623387d9225ae81bbd201124a71df2f27
[Min-hye-Kim] <kimilu@ewha.ac.kr>
2025-07-25 01:29:54 +0900

    1.6 작성중

---
+++ CH_1_6/priority_calculator.py
@@ -0,0 +1,11 @@
+from CH_1_3_to_1_4.david.calculator import add, subtract
+# 헉 파이썬에서 .와 -는 모듈 이름으로 안 되는구나.. 숫자로 시작하는 이름도 안 된다고 한다.
+# 이렇게 하려면 파이썬을 프로젝트 최상위 폴더 기준으로 실행해야 한다고 함.
+
+def main():
+    return
+
+
+
+if __name__ == "__main__":
+    main()
